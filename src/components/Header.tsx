433b477019c2e234fde15006601d9dc6bb432f8f
samiha-mahin <samihamahin36@gmail.com>
2025-09-07 01:38:30 +0600

    ecobin

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { CHAIN_NAMESPACES, IProvider, WEB3AUTH_NETWORK } from "@web3auth/base"
 import { EthereumPrivateKeyProvider } from "@web3auth/ethereum-provider"
 
-
+const clientId = process.env.WEB3_AUTH_CLIENT_ID
 const Header = () => {
   return (
     <div>
