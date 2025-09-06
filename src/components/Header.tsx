7b4dfc9d480aac98b89294474fec39ff404cee14
samiha-mahin <samihamahin36@gmail.com>
2025-09-07 01:39:18 +0600

    ecobin

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,12 +1,38 @@
-import React from 'react'
+'use client'
+import { Web3Auth } from "@web3auth/modal"
 import { CHAIN_NAMESPACES, IProvider, WEB3AUTH_NETWORK } from "@web3auth/base"
 import { EthereumPrivateKeyProvider } from "@web3auth/ethereum-provider"
 
-const clientId = process.env.WEB3_AUTH_CLIENT_ID
+const clientId = process.env.NEXT_PUBLIC_WEB3_AUTH_CLIENT_ID
+
+const chainConfig = {
+  chainNamespace: CHAIN_NAMESPACES.EIP155,
+  chainId: "0xaa36a7",
+  rpcTarget: "https://rpc.ankr.com/eth_sepolia",
+  displayName: "Ethereum Sepolia Testnet",
+  blockExplorerUrl: "https://sepolia.etherscan.io",
+  ticker: "ETH",
+  tickerName: "Ethereum",
+  logo: "https://cryptologos.cc/logos/ethereum-eth-logo.png",
+};
+const privateKeyProvider = new EthereumPrivateKeyProvider({
+  config: { chainConfig },
+});
+
+const web3auth = new Web3Auth({
+  clientId,
+  web3AuthNetwork: WEB3AUTH_NETWORK.TESTNET, // Changed from SAPPHIRE_MAINNET to TESTNET
+  privateKeyProvider,
+});
+
+interface HeaderProps {
+  onMenuClick: () => void;
+  totalEarnings: number;
+}
 const Header = () => {
   return (
     <div>
-        hellooooo
+        hellooo
     </div>
   )
 }
